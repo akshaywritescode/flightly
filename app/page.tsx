db4f52f0dec66fd31cbec7dec6c3976e7e545d80
Akshay <teamakshayyadav@gmail.com>
2026-01-17 09:16:25 +0530

    add flightSearchCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import FlightSearchCard from "./components/layout/FlightSearchCard";
 import Header from "./components/layout/header";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="h-screen bg-[#edebeb]">
-        
+      <main className="h-screen bg-[#edebeb] px-10 py-7">
+        <FlightSearchCard />
       </main>
     </>
   );
