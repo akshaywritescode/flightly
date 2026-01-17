ab1ef8444979e5dbd3d3acc911c14bacb620fb87
Akshay <teamakshayyadav@gmail.com>
2026-01-17 21:33:02 +0530

    made route accept currency params

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
 import FlightSearchCard from "./components/layout/FlightSearchCard";
+import FlightsResultSection from "./components/layout/FlightsResultSection";
 import Header from "./components/layout/header";
 
 export default function Home() {
   return (
     <>
+<<<<<<< Updated upstream
       <div className="h-screen w-auto">
         <Header />
         <main className="m-auto min-h-screen max-w-[1200px] bg-[#edebeb] px-10 pt-24">
           <FlightSearchCard />
         </main>
       </div>
+=======
+      <Header />
+      <main className="h-screen bg-[#edebeb] px-10 py-7">
+        <FlightSearchCard />
+        <FlightsResultSection />
+      </main>
+>>>>>>> Stashed changes
     </>
   );
 }
