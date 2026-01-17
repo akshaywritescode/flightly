56ec4882f0d000edd657afd0d197f1f9160f3e85
Akshay <teamakshayyadav@gmail.com>
2026-01-17 09:11:18 +0530

    add search flight button

---
+++ app/components/ui/SearchFlightBtn.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+
+export default function SearchFlightBtn(){
+    return <Button className="text-xs bg-blue-600 cursor-pointer hover:bg-blue-700">
+        Search Flights
+    </Button>
+}
