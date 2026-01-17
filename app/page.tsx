3d01ac0bd9cd28d996c2c3a47d3d10761b16c820
Akshay <teamakshayyadav@gmail.com>
2026-01-17 21:34:28 +0530

    add flight result section component

--- app/page.tsx
+++ app/page.tsx
@@ -5,20 +5,13 @@ import Header from "./components/layout/header";
 export default function Home() {
   return (
     <>
-<<<<<<< Updated upstream
       <div className="h-screen w-auto">
         <Header />
         <main className="m-auto min-h-screen max-w-[1200px] bg-[#edebeb] px-10 pt-24">
           <FlightSearchCard />
+          <FlightsResultSection />
         </main>
       </div>
-=======
-      <Header />
-      <main className="h-screen bg-[#edebeb] px-10 py-7">
-        <FlightSearchCard />
-        <FlightsResultSection />
-      </main>
->>>>>>> Stashed changes
     </>
   );
 }
