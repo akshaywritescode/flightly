066343c52ac76165008a258475d77845eda7b42b
Akshay <teamakshayyadav@gmail.com>
2026-01-17 22:31:57 +0530

    add trip typec selector to select oneway/round trip

--- app/components/layout/FlightSearchCard.tsx
+++ app/components/layout/FlightSearchCard.tsx
@@ -3,17 +3,20 @@ import { FromLocationInput } from "../ui/FromLocationInput";
 import { ToLocationInput } from "../ui/ToLocationInput";
 import { ArrowLeftRight } from "lucide-react";
 import SearchFlightBtn from "../ui/SearchFlightBtn";
+import { TripTypeSelector } from "../ui/TripTypeSwitcher";
 
 export default function FlightSearchCard() {
   return (
-    <Card className="rounded-xl p-6 flex justify-between">
-        
-      <div className="flex items-center gap-5">
-        <FromLocationInput />
-        <ArrowLeftRight size={18} className="text-black/70" />
-        <ToLocationInput />
+    <Card className="rounded-xl p-6 flex flex-col gap-4">
+      <div className="w-full flex justify-between">
+        <div className="flex items-center gap-5">
+          <FromLocationInput />
+          <ArrowLeftRight size={18} className="text-black/70" />
+          <ToLocationInput />
+        </div>
+        <SearchFlightBtn />
       </div>
-      <SearchFlightBtn />
+      <TripTypeSelector />
     </Card>
   );
 }
---
+++ app/components/ui/TripTypeSwitcher.tsx
@@ -0,0 +1,21 @@
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { Label } from "@/components/ui/label"
+
+export function TripTypeSelector() {
+  return (
+    <RadioGroup
+      defaultValue="oneway"
+      className="flex gap-6"
+    >
+      <div className="flex items-center gap-2">
+        <RadioGroupItem value="oneway" id="oneway" />
+        <Label htmlFor="oneway">One way</Label>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <RadioGroupItem value="roundtrip" id="roundtrip" />
+        <Label htmlFor="roundtrip">Round Trip</Label>
+      </div>
+    </RadioGroup>
+  )
+}
