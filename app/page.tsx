2b4c89ff709953c1e92b14d024913d5f958e48aa
Akshay <teamakshayyadav@gmail.com>
2026-01-17 21:17:07 +0530

    made main section center with max-width 1200px

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import Header from "./components/layout/header";
 export default function Home() {
   return (
     <>
-      <Header />
-      <main className="h-screen bg-[#edebeb] px-10 py-7">
-        <FlightSearchCard />
-      </main>
+      <div className="h-screen w-auto">
+        <Header />
+        <main className="m-auto min-h-screen max-w-[1200px] bg-[#edebeb] px-10 pt-24">
+          <FlightSearchCard />
+        </main>
+      </div>
     </>
   );
 }
