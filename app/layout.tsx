6e1739b5002f929b47ff627c1ce35bcb5da40729
Akshay <teamakshayyadav@gmail.com>
2026-01-18 07:50:56 +0530

    fix space on fon declaration

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${poppins.className}w-auto bg-[#edebeb] antialiased`}
+        className={`${poppins.className} w-auto bg-[#edebeb] antialiased`}
       >
         <ThemeProvider
             attribute="class"
